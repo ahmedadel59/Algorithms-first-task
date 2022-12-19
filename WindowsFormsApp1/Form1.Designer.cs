8b16c028fd0a03bbf751d049763f9c1e6c0f9145
ahmedadel59 <112099146+ahmedadel59@users.noreply.github.com>
2022-12-19 18:10:39 +0200

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.bn = new System.Windows.Forms.Button();
+            this.bn2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the elements of the array :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(265, 67);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(100, 20);
+            this.txt.TabIndex = 1;
+            this.txt.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            // 
+            // bn
+            // 
+            this.bn.BackColor = System.Drawing.Color.Lime;
+            this.bn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bn.Location = new System.Drawing.Point(393, 123);
+            this.bn.Name = "bn";
+            this.bn.Size = new System.Drawing.Size(130, 30);
+            this.bn.TabIndex = 2;
+            this.bn.Text = "Insertion Sort";
+            this.bn.UseVisualStyleBackColor = false;
+            this.bn.Click += new System.EventHandler(this.bn_Click);
+            // 
+            // bn2
+            // 
+            this.bn2.BackColor = System.Drawing.Color.BlueViolet;
+            this.bn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bn2.Location = new System.Drawing.Point(401, 67);
+            this.bn2.Name = "bn2";
+            this.bn2.Size = new System.Drawing.Size(75, 23);
+            this.bn2.TabIndex = 3;
+            this.bn2.Text = "Add";
+            this.bn2.UseVisualStyleBackColor = false;
+            this.bn2.Click += new System.EventHandler(this.bn2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(321, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(265, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 30);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Merge Sort";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bn2);
+            this.Controls.Add(this.bn);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt;
+        private System.Windows.Forms.Button bn;
+        private System.Windows.Forms.Button bn2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
+using System.Threading;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int[] arr = new int[1000];
+        int count = 0;
+        bool isNotZero(int n)
+        {
+            return n != 0;
+        }
+        
+        void InsertionSort(int[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (inputArray[j - 1] > inputArray[j])
+                    {
+                        int temp = inputArray[j - 1];
+                        inputArray[j - 1] = inputArray[j];
+                        inputArray[j] = temp;
+                    }
+                }
+            }
+            
+        }
+        public void MergeArray(int[] array, int left, int middle, int right)
+        {
+            var leftArrayLength = middle - left + 1;
+            var rightArrayLength = right - middle;
+            var leftTempArray = new int[leftArrayLength];
+            var rightTempArray = new int[rightArrayLength];
+            int i, j;
+            for (i = 0; i < leftArrayLength; ++i)
+                leftTempArray[i] = array[left + i];
+            for (j = 0; j < rightArrayLength; ++j)
+                rightTempArray[j] = array[middle + 1 + j];
+            i = 0;
+            j = 0;
+            int k = left;
+            while (i < leftArrayLength && j < rightArrayLength)
+            {
+                if (leftTempArray[i] <= rightTempArray[j])
+                {
+                    array[k++] = leftTempArray[i++];
+                }
+                else
+                {
+                    array[k++] = rightTempArray[j++];
+                }
+            }
+            while (i < leftArrayLength)
+            {
+                array[k++] = leftTempArray[i++];
+            }
+            while (j < rightArrayLength)
+            {
+                array[k++] = rightTempArray[j++];
+            }
+        }
+        public int[] SortArray(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = left + (right - left) / 2;
+                SortArray(array, left, middle);
+                SortArray(array, middle + 1, right);
+                MergeArray(array, left, middle, right);
+            }
+            return array;
+        }
+      
+
+        
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bn2_Click(object sender, EventArgs e)
+        {
+          
+            arr[count] = Convert.ToInt32(txt.Text);
+            count++;
+            txt.Clear();
+        }
+       
+        private void txt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bn_Click(object sender, EventArgs e)
+        {
+            
+            InsertionSort(arr);
+            arr = Array.FindAll(arr, isNotZero).ToArray();
+            string toDisplay = string.Join(Environment.NewLine, arr);
+            MessageBox.Show(toDisplay);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SortArray(arr, 0, arr.Length - 1);
+            arr = Array.FindAll(arr, isNotZero).ToArray();
+            string toDisplay = string.Join(Environment.NewLine, arr);
+            MessageBox.Show(toDisplay);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
